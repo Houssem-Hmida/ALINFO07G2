68d008b3d0ebc7f87669cc6ac6570312f8a8b485
Rahma Messadi <rahma.messadi@avaxia-group.com>
2023-02-13 15:18:04 +0100

    tt

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+package tn.esprit.alternance;
+
+import java.util.Scanner;
+
+public class Multiplication {
+//
+	//git
+	public static void multiplication()
+	{int nOne, nTwo;
+	Scanner kb = new Scanner(System.in);
+	System.out.println("Multiplication");
+	System.out.print("\nFirst Number: ");nOne = kb.nextInt();
+	System.out.print("\nSecond Number: ");nTwo = kb.nextInt();
+	kb.close();System.out.println("\nSum: " + nOne + " x " + nTwo + " = " + (nOne * nTwo));}
+}
